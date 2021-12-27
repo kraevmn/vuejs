3a2566442300590b558bdc9a15ea5674a10d903f
Mikhail <mkraev@corp.gorodrabot.ru>
2021-12-27 16:16:00 +0300

    vue lesson 5

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 new Vue({
     el: "#app",
     data: {
-        people: [
-            {
-                name: "Franky",
-                stillAlive: true
-            },
-            {
-                name: "Franky2",
-                stillAlive: false
-            },
-            {
-                name: "Franky3",
-                stillAlive: true
-            },
-            {
-                name: "Franky4",
-                stillAlive: false
-            },
-            {
-                name: "Franky5",
-                stillAlive: true
-            },
-        ]
+        counter: 0
+    },
+    methods: {
+        substract: function(){
+            this.counter--;
+        }
     }
-
 })

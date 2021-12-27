d7b5287f4da7be49d9a196302c437d581ed7a158
Mikhail <mkraev@corp.gorodrabot.ru>
2021-12-27 16:04:55 +0300

    vue lesson 4

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 new Vue({
     el: "#app",
     data: {
-        link: "http://example.com",
-        markup: '<a href=http://example.com>!!!!!!!!</a>',
-        name: 'Max'
+        people: [
+            {
+                name: "Franky",
+                stillAlive: true
+            },
+            {
+                name: "Franky2",
+                stillAlive: false
+            },
+            {
+                name: "Franky3",
+                stillAlive: true
+            },
+            {
+                name: "Franky4",
+                stillAlive: false
+            },
+            {
+                name: "Franky5",
+                stillAlive: true
+            },
+        ]
     }
 
 })

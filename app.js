cbb5c9bf4d073ac4fbcf6802d091551a28f72946
Mikhail <mkraev@corp.gorodrabot.ru>
2021-12-29 17:04:48 +0300

    1

--- app.js
+++ app.js
@@ -1,11 +1,60 @@
 new Vue({
     el: "#app",
     data: {
-        counter: 0
+        currentTask: '',
+        editValue: '',
+        tasks: [
+            {
+                text: "Subscribe",
+                isComplited: true,
+                isEditing: false
+            },
+            {
+                text: "Like",
+                isComplited: true,
+                isEditing: false
+            },
+            {
+                text: "Learn",
+                isComplited: false,
+                isEditing: false
+            }
+
+        ]
     },
     methods: {
-        substract: function(){
-            this.counter--;
-        }
+        addTask: function(){
+            this.tasks.push({
+                text: this.currentTask,
+                isComplited: false,
+                isEditing: false
+            });
+            this.currentTask = '';
+        },
+        removeTask: function(taskText){
+            this.tasks = this.tasks.filter(task => {
+                return task.text !== taskText;
+            })
+        },
+        changeEditing: function(taskText) {
+            this.editValue = taskText;
+            this.tasks = this.tasks.map(task => {
+                if (task.text === taskText) {
+                    task.isEditing = !task.isEditing;
+                }
+                return task;
+            })
+        },
+        editTask: function(taskText){
+            this.tasks = this.tasks.map(task => {
+                if (task.text === taskText) {
+                    task.isEditing = !task.isEditing;
+                    task.text = this.editValue;
+                }
+                return task;
+            })
+
+        },        
+        
     }
 })
